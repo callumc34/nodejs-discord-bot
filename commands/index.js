d2469d93a89ab4174c00c2c156e8107d37017a0d
Callum C <cpcooper34@hotmail.com>
2021-03-20 19:06:55 +1300

    fix: Reverted addCommand parameters

--- commands/index.js
+++ commands/index.js
@@ -73,7 +73,7 @@ const CommandCollection = class CommandCollection extends EventEmitter {
          * @param {Command} command - command to run
          * @return {Boolean} - true returned
          */
-        addCommand (command) {
+        addCommand (name, command) {
             if (!Command.isPrototypeOf(command.constructor)) {
                 //Check that class inherits from Command
                 return false;
@@ -81,7 +81,7 @@ const CommandCollection = class CommandCollection extends EventEmitter {
             command.on("ran", (ctx, args, result, command) => {
                 this.emit("ran", ctx, args, result, command);
             });
-            this._commands[command.name] = command;
+            this._commands[name] = command;
             return true;
         }
 
